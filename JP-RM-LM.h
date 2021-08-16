180a09474e85ace8d88d094686ea233dc7fb6bdd
RodrigoMato00 <3293@holbertonschool.com>
2021-08-16 12:30:26 -0700

    shell

---
+++ JP-RM-LM.h
@@ -0,0 +1,56 @@
+#ifndef JP_RM_LM_H
+#define JP_RM_LM_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <signal.h>
+
+/**
+ *struct direc - structure for the implementation of Linkedlist (PATH)
+ *@a: directory path
+ *@next: pointer a next node
+ */
+typedef struct direc
+{
+	char * jeje;
+	struct direc * next;
+} PDIRECT;
+
+/**
+ *struct chdirect - structure for function cd
+ *@jeje: directory path
+ *@cde: indicated to cd calling
+ */
+typedef struct chdirect
+{
+	char *jeje;
+	int cde;
+} CHDIRECT;
+
+char *_getenv(const char *name);
+int checkenv(char **p);
+int main(int argc __attribute__ ((unused)), char **argv);
+char *_str_token(char *jeje, char *d);
+int str_len(char *jeje);
+int counter(char *jeje);
+void _itoa(int a, char *c);
+int changedir(char **p, CHDIRECT *predirect);
+int _strcmp(char *s1, char *s2);
+char *_strdup(char *jeje);
+char *findcommand(PDIRECT *head, char *commandinput);
+void _itoa(int a, char *c);
+PDIRECT *linkedpath(void);
+int _atoi(char *jeje);
+int checkexit(char **token);
+void __exit(int errnum, char **p, char *getline, PDIRECT *head, char *fc, char *predirect);
+void errmessage(char **c, char *p, int i);
+void CDerrmessage(char **c, char *p, int i);
+extern char **environ;
+
+#endif
---
+++ cd.c
@@ -0,0 +1,57 @@
+#include "JP-RM-LM.h"
+
+/**
+ * changedir - changes directory
+ * @p: p user entered commands
+ * @predirect: previous directory
+ * Return: zdkmadmcp
+ */
+int changedir(char **p, CHDIRECT *predirect)
+{
+	char cd[] = "cd";
+	char tilde[] = "~";
+	char dash[] = "-";
+	char *jeje;
+	char *temp;
+
+	if (_strcmp(p[0], cd) == 0)
+	{
+
+		predirect->cde = 1;
+		if (p[1] == NULL ||  _strcmp(p[1], tilde) == 0)
+		{
+
+			jeje = _getenv("HOME");
+			free(predirect->jeje);
+			predirect->jeje = _strdup(_getenv("PWD"));
+			chdir(jeje);
+
+
+		}
+		else if (_strcmp(p[1], dash) == 0)
+		{
+			temp = _strdup(_getenv("PWD"));
+			chdir(predirect->jeje);
+			write(STDOUT_FILENO, predirect->jeje, str_len(predirect->jeje));
+			write(STDOUT_FILENO, "\n", 1);
+			free(predirect->jeje);
+			predirect->jeje = temp;
+		}
+		else
+		{
+			temp = _strdup(_getenv("PWD"));
+			if (chdir(p[1]) == -1)
+			{
+				free(temp);
+				return (-1);
+			}
+			else
+			{
+				free(predirect->jeje);
+				predirect->jeje = temp;
+
+			}
+		}
+	}
+	return (0);
+}
